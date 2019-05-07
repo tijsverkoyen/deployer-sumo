0cd6258c1c303e7eddd5b762a07032bd310d376f
Tijs Verkoyen <tijs@verkoyen.eu>
2019-05-07 16:28:02 +0200

    Expect the autoloader to be already loaded

--- sumo.php
+++ sumo.php
@@ -2,8 +2,6 @@
 
 namespace Deployer;
 
-require __DIR__ . '/vendor/autoload.php';
-
 require_once 'recipe/database.php';
 require_once 'recipe/notifications.php';
 require_once 'recipe/redirect.php';
