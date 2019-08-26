351c6649d116740ff975ba613a3726a113cf9337
Tijs Verkoyen <tijs@verkoyen.eu>
2019-08-26 17:20:29 +0200

    Added empty tasks
    This is the very minimal implementation of the tasks. The real code should be
    implemented in a following commit.
    
    With this the tasks are shown in the list of available tasks.

---
+++ recipe/files.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Deployer;
+
+desc('Replace the local files with the remote files');
+task(
+    'sumo:files:get',
+    function () {
+    }
+);
+
+desc('Replace the remote files with the local files');
+task(
+    'sumo:files:put',
+    function () {
+    }
+);
