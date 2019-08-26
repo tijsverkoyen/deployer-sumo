0681179f2da1b37b7a4cdb2db1f0a71d26d338ce
Tijs Verkoyen <tijs@verkoyen.eu>
2019-08-26 17:22:23 +0200

    Implemented sumo:files:get
    The real implementation for the get-task. It will loop all shared directories
    and download the content to the local machine.

--- recipe/files.php
+++ recipe/files.php
@@ -6,6 +6,18 @@ desc('Replace the local files with the remote files');
 task(
     'sumo:files:get',
     function () {
+        $sharedDirectories = get('shared_dirs');
+        if (!is_array($sharedDirectories) || empty($sharedDirectories)) {
+            return;
+        }
+
+        foreach ($sharedDirectories as $directory) {
+            $path = '{{deploy_path}}/shared/' . $directory;
+
+            if (test(sprintf('[ -d %1$s ]', $path))) {
+                download($path, './');
+            }
+        }
     }
 );
 
