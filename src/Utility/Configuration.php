33f7b6fff3cd1042483086d408b892c6072ef43f
Davy De Vuysdere <davy@sumocoders.be>
2021-09-24 14:51:31 +0200

    check if parameters.yml file exists and build databaseUrl from them

--- src/Utility/Configuration.php
+++ src/Utility/Configuration.php
@@ -4,6 +4,7 @@ namespace TijsVerkoyen\DeployerSumo\Utility;
 
 use Deployer;
 use Symfony\Component\Dotenv\Dotenv;
+use Symfony\Component\Yaml\Yaml;
 
 class Configuration
 {
@@ -37,6 +38,18 @@ class Configuration
     {
         $values = [];
 
+        // check if we have parameters.yml file
+        $envPath = Deployer\get('deploy_path') . '/shared/app/config/parameters.yml';
+
+        if (Deployer\test("[ -f $envPath ]")) {
+            // get database url from parameters
+            $parameters = Yaml::parse(Deployer\run('cat app/config/parameters.yml'))['parameters'];
+            $databaseUrl = self::getDatabaseUrlFromParameters($parameters);
+            if ($databaseUrl !== '') {
+                return new Configuration(['DATABASE_URL' => $databaseUrl]);
+            }
+        }
+
         // check if we have a .env.local or .env file
         $envPath = Deployer\get('deploy_path') . '/shared/.env';
         if (Deployer\test("[ -f $envPath.local ]")) {
@@ -69,5 +82,21 @@ class Configuration
         );
 
         return new Configuration($values);
+    }
+    
+    private static function getDatabaseUrlFromParameters(array $parameters): string
+    {
+        if (!array_key_exists('database.host', $parameters)) {
+            return '';
+        }
+        if (!array_key_exists('database.user', $parameters)) {
+            return '';
+        }
+        if (!array_key_exists('database.password', $parameters)) {
+            return '';
+        }
+
+        return "mysql://{$parameters['database.user']}:{$parameters['database.password']}" .
+            "@{$parameters['database.host']}:{$parameters['database.port']}/{$parameters['database.name']}";
     }
 }
