b1ea43c43ffd6f0f07725959c1cd0b9ee10d0f98
Sander De la Marche <sander@sumocoders.be>
2022-02-01 10:58:52 +0100

    Fix typo

--- recipe/project.php
+++ recipe/project.php
@@ -6,14 +6,11 @@ desc('Get the project from the specified host.');
 task(
     'sumo:project:get',
     function () {
-        invoke('sumo:db:create-locale');
+        invoke('sumo:db:create-local');
         invoke('sumo:db:get');
         invoke('sumo:config:alter');
         invoke('sumo:files:get');
-        invoke('sumo:assets:fix-npm'); // niet nodig? nvm exec doet ook install?
+        invoke('sumo:assets:fix-npm');
         invoke('sumo:assets:build');
     }
 );
-
-// TODO: oudated?
-before('deploy:symlink', 'sumo:assets:install');
