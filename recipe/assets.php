c158a1b1d7cd56b8f0649da7943fa41d9cb2b337
Sander De la Marche <sander@sumocoders.be>
2022-03-11 10:09:24 +0100

    Fix the assets upload path.
    With the __DIR__ included, we'd uploa the /public/build of the
    vendor folder of this package, and not the build folder of our
    project.

--- recipe/assets.php
+++ recipe/assets.php
@@ -6,7 +6,44 @@ desc('Install bundle\'s web assets under a public directory');
 task(
     'sumo:assets:install',
     function () {
-        run('{{bin/console}} assets:install {{console_options}}');
+        $nvmPath = trim(shell_exec('echo $HOME/.nvm/nvm.sh'));
+
+        if (!file_exists($nvmPath)) {
+            writeln('Nvm not found on local system. Aborting');
+            return;
+        }
+
+        $nvmRcFile = '.nvmrc';
+
+        // If there is no .nvmrc file, stop
+        if (!file_exists($nvmRcFile)) {
+            writeln('No .nvmrc file found. Aborting.');
+            return;
+        }
+
+        writeln(runLocally('. ' . $nvmPath . ' && nvm install'));
+    }
+);
+
+desc('Run the build script which will build our needed assets.');
+task(
+    'sumo:assets:build',
+    function () {
+        $nvmPath = trim(shell_exec('echo $HOME/.nvm/nvm.sh'));
+
+        if (file_exists($nvmPath)) {
+            runLocally('. ' . $nvmPath . ' && nvm use && nvm exec npm run build');
+        } else {
+            runLocally('npm run build');
+        }
+    }
+);
+
+desc('Uploads the assets');
+task(
+    'sumo:assets:upload',
+    function () {
+        upload('/public/build', '{{release_path}}/public');
     }
 );
 
