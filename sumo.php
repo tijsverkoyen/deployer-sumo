4dc3c62812a98fa775f5695099fd186469b243aa
Tijs Verkoyen <tijs@verkoyen.eu>
2022-05-04 13:58:29 +0200

    Require our Utility classes manually
    It seems like deployer does not do autoloading.

--- sumo.php
+++ sumo.php
@@ -2,6 +2,11 @@
 
 namespace Deployer;
 
+require_once 'src/Utility/Configuration.php';
+require_once 'src/Utility/Database.php';
+require_once 'src/Utility/Git.php';
+require_once 'src/Utility/Path.php';
+
 require_once 'recipe/assets.php';
 require_once 'recipe/database.php';
 require_once 'recipe/config.php';
