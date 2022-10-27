90ced1df438a0658488706ac567c08e91b9326c3
Abraham <abraham@sumocoders.be>
2022-10-27 11:59:22 +0200

    Check if volta is used in command fix-node-version

--- recipe/assets.php
+++ recipe/assets.php
@@ -6,6 +6,17 @@ desc('Run the build script which will build our needed assets.');
 task(
     'sumo:assets:fix-node-version',
     function () {
+        if (!shell_exec('command -v volta')) {
+            writeln('Volta not found on local system.');
+        }
+
+        if (isset(json_decode(file_get_contents('package.json') ?: '', true)['volta'])) {
+            return;
+        }
+
+        warning('Switching to Volta is recommended.');
+        writeln('Run `volta pin node@lts && rm .nvmrc` and commit');
+
         $nvmPath = trim(shell_exec('echo $HOME/.nvm/nvm.sh'));
 
         if (!file_exists($nvmPath)) {
@@ -31,7 +42,7 @@ task(
     function () {
         $nvmPath = trim(shell_exec('echo $HOME/.nvm/nvm.sh'));
 
-        if (file_exists($nvmPath)) {
+        if (file_exists($nvmPath) && file_exists('.nvmrc')) {
             runLocally('. ' . $nvmPath . ' && nvm use && nvm exec npm run build');
         } else {
             runLocally('npm run build');
