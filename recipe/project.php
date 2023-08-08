ac14cd3f80fe1788cf6e5d644dd077c117f6d857
Abraham <abraham@sumocoders.be>
2023-08-08 14:38:48 +0200

    The database url is saved in the config file
    Get the config file before creating the database

--- recipe/project.php
+++ recipe/project.php
@@ -6,10 +6,10 @@ desc('Get the project from the specified host.');
 task(
     'sumo:project:init',
     function () {
-        invoke('sumo:db:create-local');
-        invoke('sumo:db:get');
         invoke('sumo:config:get');
         invoke('sumo:config:alter');
+        invoke('sumo:db:create-local');
+        invoke('sumo:db:get');
         invoke('sumo:files:get');
         invoke('sumo:assets:fix-node-version');
         invoke('sumo:assets:npm-install');
